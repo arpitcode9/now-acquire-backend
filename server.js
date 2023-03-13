a1e2ca08f83042e894609006ba5efc6927f47837
arpitcode9 <125914452+arpitcode9@users.noreply.github.com>
2023-03-13 18:33:19 +0530

    Testing something to fix cors error

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const app = express();
 //   origin: ["https://now-acquire-frontend.vercel.app/", "https://localhost:8080/", "https://app.getpostman.com/" , "https://now-acquire-frontend-34sx.vercel.app/" , "nowacquire.com"]
 // };
 
-app.use(cors());
+// allow all
+var corsOptions = {
+ origin: ["*"]
+};
+
+app.use(cors(corsOptions));
 
 //app.use(bodyParser.urlencoded({ extended: true }))
 
